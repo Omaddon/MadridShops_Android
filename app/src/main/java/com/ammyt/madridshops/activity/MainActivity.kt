fb6f1205345e903872e92c290ff63a4920e3c1f5
Miguel Jardón <migueljp83@gmail.com>
2018-02-06 16:51:00 +0100

    GoogleMap init setup.

--- app/src/main/java/com/ammyt/madridshops/activity/MainActivity.kt
+++ app/src/main/java/com/ammyt/madridshops/activity/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.ammyt.madridshops.activity
 
 import android.os.Bundle
-import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import com.ammyt.domain.interactor.ErrorCompletion
+import com.ammyt.domain.interactor.SuccessCompletion
+import com.ammyt.domain.interactor.getallshops.GetAllShopsInteractor
+import com.ammyt.domain.interactor.getallshops.GetAllShopsInteractorImpl
+import com.ammyt.domain.model.Shops
 import com.ammyt.madridshops.R
+import com.ammyt.madridshops.fragment.ListFragment
+import com.google.android.gms.maps.SupportMapFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var listFragment: ListFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +27,32 @@ class MainActivity : AppCompatActivity() {
 
         Log.d("App", "onCreate MainActivity")
 
+        setupMap()
+
+        listFragment = supportFragmentManager.findFragmentById(R.id.activity_main_list_fragment) as ListFragment
+
+    }
+
+    private fun setupMap() {
+        var getAllShopsInteractor: GetAllShopsInteractor = GetAllShopsInteractorImpl(this)
+        getAllShopsInteractor.execute(object: SuccessCompletion<Shops> {
+            override fun successCompletion(shops: Shops) {
+                initializeMap()
+            }
+
+        }, object: ErrorCompletion {
+            override fun errorCompletion(errorMessage: String) {
+                Toast.makeText(baseContext, "Error loading.", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+
+    private fun initializeMap() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.activity_main_map_fragment) as SupportMapFragment
+        mapFragment.getMapAsync({
+            Log.d("SUCCESS", "HABEMUS MAPA")
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
