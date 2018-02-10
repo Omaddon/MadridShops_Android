51fa093beb05b7ca6af2c8d3d110ae7375b1eeee
Miguel Jardón <migueljp83@gmail.com>
2018-02-10 21:17:25 +0100

    ShopDetail DONE. TODO: language, refresh map pins image, "mainView".

--- app/src/main/java/com/ammyt/madridshops/activity/ShopDetailActivity.kt
+++ app/src/main/java/com/ammyt/madridshops/activity/ShopDetailActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import com.ammyt.domain.model.Shop
 import com.ammyt.madridshops.R
 import com.ammyt.madridshops.utils.INTENT_SHOP_DETAIL
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_shop_detail.*
 
 class ShopDetailActivity : AppCompatActivity() {
@@ -15,6 +16,25 @@ class ShopDetailActivity : AppCompatActivity() {
 
         val shop = intent.getSerializableExtra(INTENT_SHOP_DETAIL) as Shop
 
-        testing_text.text =  shop.name
+        // TODO comprobar idioma a mostrar
+        shop_name.text =  shop.name
+        shop_description.text = shop.description_es
+        shop_address.text = shop.address
+        shop_hours.text = shop.openingHours_es
+
+        Picasso
+                .with(this)
+                .load(shop.imageURL)
+                .placeholder(R.drawable.no_image)
+                .into(shop_image)
+
+        val googleMap_url =
+                "https://maps.googleapis.com/maps/api/staticmap?%25&size=320x220&scale=2&markers=" + shop.latitude + "," + shop.longitude
+
+        Picasso
+                .with(this)
+                .load(googleMap_url)
+                .placeholder(R.drawable.no_image)
+                .into(shop_google_map)
     }
 }
