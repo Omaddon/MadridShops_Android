6f8b08e40b1dcfe5dc40b2b7420ffcfa1d838afd
Miguel Jardón <migueljp83@gmail.com>
2018-02-12 14:17:36 +0100

    activityDetaik WIP. TODO: internetConnectionStatus.

--- domain/src/main/java/com/ammyt/domain/interactor/internetstatus/InternetStatusInteractorImpl.kt
+++ domain/src/main/java/com/ammyt/domain/interactor/internetstatus/InternetStatusInteractorImpl.kt
@@ -2,17 +2,24 @@ package com.ammyt.domain.interactor.internetstatus
 
 import android.content.Context
 import android.net.ConnectivityManager
-import com.ammyt.domain.BuildConfig
 import com.ammyt.domain.interactor.CodeClosure
 import com.ammyt.domain.interactor.ErrorClosure
-import com.ammyt.madridshops.repository.thread.DispatchOnMainThread
-import java.io.IOException
-import java.net.HttpURLConnection
-import java.net.URL
 
 // TODO implementar internetStatus
 class InternetStatusInteractorImpl : InternetStatusInteractor {
     override fun execute(context: Context, success: CodeClosure, error: ErrorClosure) {
-        success()
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+
+        if (connectivityManager != null) {
+            val netInfo = connectivityManager.activeNetworkInfo
+
+            if (netInfo != null && netInfo.isConnected) {
+                success()
+            } else {
+                error("Conexion error. Unable connect to server.")
+            }
+        } else {
+            error("Conexion error. Unable connect to server.")
+        }
     }
 }
